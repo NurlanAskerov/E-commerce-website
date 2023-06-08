55de83b26917e28b52eaa835cdb290a91bd68e29
Nurlan <124917278+NurlanAskerov@users.noreply.github.com>
2023-06-08 10:47:37 +0400

    Update ProductDetail.jsx

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import fetchApi from './fetchApi'
 import AddToCart from './AddToCart'
 import CardContext from '../useContext/CardContext'
-
+import { ToastContainer } from 'react-toastify';
 
 const ProductDetail = () => {
     const [product,setProduct]=useState({})
@@ -30,17 +30,18 @@ const ProductDetail = () => {
     // };
   return (
     <div className='mt-36 m-20'>
+      <ToastContainer/>
       <div className="flex max-lg:flex-col ">
           <div className="flex w-1/2 justify-center items-center max-lg:w-full">
         <img className='max-w-[200px] m-10' src={product.image}/>
-        </div>
+        </div>          
           <div className="product-details p-6">
             <h1 className='text-2xl font-bold py-2'>{product.title}</h1>
              <p className='text-sm opacity-90 py-2'>Category : {product.category}</p>
              <h3 className='font-bold py-2'>{product.description}</h3>
            <button className='px-3 py-2 text-white bg-black my-3' onClick={()=>AddToCart(product,setCardItems, cardItems, total, setTotal)}>Add to cart</button>
           </div>
-      </div>
+      </div> 
       </div>
   )
 }
