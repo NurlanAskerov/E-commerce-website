ecf20d6926478cf04be60399f57c720191fe41e4
Nurlan Askerov <nurlaneskerov000@gmail.com>
2023-06-09 14:26:55 +0400

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='bg-gray-900 h-[25vh] w-[100vw] mt-36 text-white flex justify-center items-end'><h6 className='mb-2 p-3 pb-14 text-lg'>© 2023 Designed and coded by Nurlan Askerov</h6></div>
-  )
-}
+    <div className="bg-gray-900 min-h-[40vh] w-[100vw] mt-36 text-white flex justify-center items-end">
+      <h6 className="mb-2 p-3 pb-14 text-lg">
+        © 2023 Designed and coded by Nurlan Askerov
+      </h6>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
