b3195feb0bdeec5b54513f114b05d08e3c50dbee
Nurlan Askerov <nurlaneskerov000@gmail.com>
2023-06-09 12:51:37 +0400

    loading updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Products from "./Products";
-import HeroImage from "../../public/DrawKit-Vector-Illustration-ecommerce-01 (1).svg"
+import HeroImage from "../../public/DrawKit-Vector-Illustration-ecommerce-01 (1).svg";
 const Home = () => {
-   return(    <><div className="w-full h-[100vh] pt-10 mt-14 ">
-      <section>
-        <div className="container mx-auto">
-          <div className="lg:flex items-center mt-24  md:mt-10">
-            <div className="flex flex-wrap justify-center items-center  md:mx-auto">
-              <h1 className="font-bold text-center text-5xl p-4 mb-24 main-text">
-              Online Shopping<br/>App
-              </h1>
-            </div>
-            <img
-              className=" mx-auto  w-full lg:w-1/2"
-              src={HeroImage}
-            />
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const image = new Image();
+    image.onload = () => {
+      setIsLoading(false);
+    };
+    image.src = HeroImage;
+  }, []);
+  return (
+    <>
+      <div className="w-full h-[100vh] pt-10 mt-14 ">
+        <section>
+          <div className="container mx-auto">
+            {!isLoading ? (
+              <div className="lg:flex items-center mt-24  md:mt-10">
+                <div className="flex flex-wrap justify-center items-center  md:mx-auto">
+                  <h1 className="font-bold text-center text-5xl p-4 mb-24 main-text">
+                    Online Shopping
+                    <br />
+                    App
+                  </h1>
+                </div>
+                <img className=" mx-auto  w-full lg:w-1/2" src={HeroImage} />
+              </div>
+            ) : (
+              <div className="flex justify-center items-center h-[100vh] w-full text-3xl font-bold -mt-24">
+                Loading..
+              </div>
+            )}
           </div>
-        </div>
-      </section>
-      
-    </div>
-    <Products/>
+        </section>
+      </div>
+      <Products />
     </>
   );
 };
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,48 +1,47 @@
-import React, {  useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import fetchApi from './fetchApi'
-import AddToCart from './AddToCart'
-import CardContext from '../useContext/CardContext'
-
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import fetchApi from './fetchApi';
+import AddToCart from './AddToCart';
+import CardContext from '../useContext/CardContext';
 
 const ProductDetail = () => {
-    const [product,setProduct]=useState({})
-    const {productId}=useParams()
-     const { setCardItems, cardItems, total, setTotal } = useContext(CardContext);
+  const [product, setProduct] = useState({});
+  const { productId } = useParams();
+  const { setCardItems, cardItems, total, setTotal } = useContext(CardContext);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-         fetchApi(`/products/${productId}`).then(data=>setProduct(data))
-    },[productId])
-     
-    // const handleAdd = (product) => {
-    //   const exsistingItem = cardItems.find((item) => item.id === product.id);
-    //   if (exsistingItem) {
-    //     const updatedItems = cardItems.map((product) =>
-    //       product.id === exsistingItem.id
-    //         ? { ...product, quantity: product.quantity + 1 }
-    //         : product
-    //     );
-    //     setCardItems(updatedItems);
-    //   } else {
-    //     setCardItems([...cardItems, { ...product, quantity: 1 }]);
-    //   }
-    //   setTotal(total + product.price);
-    // };
+  useEffect(() => {
+    fetchApi(`/products/${productId}`).then((data) => {
+      setProduct(data);
+      setIsLoading(false);
+    });
+  }, [productId]);
+
+  const handleAddToCart = () => {
+    AddToCart(product, setCardItems, cardItems, total, setTotal);
+  };
+
   return (
     <div className='mt-36 m-20'>
-      <div className="flex max-lg:flex-col ">
-          <div className="flex w-1/2 justify-center items-center max-lg:w-full">
-        <img className='max-w-[200px] m-10' src={product.image}/>
+      {isLoading ? (
+        <div className="w-full h-[100vh] flex justify-center items-center text-2xl font-bold ">
+          Loading...
         </div>
+      ) : (
+        <div className="flex max-lg:flex-col min-h-[45vh]">
+          <div className="flex w-1/2 justify-center items-center max-lg:w-full">
+            <img className='max-w-[200px] m-10' src={product.image} alt={product.title} />
+          </div>
           <div className="product-details p-6">
             <h1 className='text-2xl font-bold py-2'>{product.title}</h1>
-             <p className='text-sm opacity-90 py-2'>Category : {product.category}</p>
-             <h3 className='font-bold py-2'>{product.description}</h3>
-           <button className='px-3 py-2 text-white bg-black my-3' onClick={()=>AddToCart(product,setCardItems, cardItems, total, setTotal)}>Add to cart</button>
+            <p className='text-sm opacity-90 py-2'>Category: {product.category}</p>
+            <h3 className='font-bold py-2'>{product.description}</h3>
+            <button className='px-3 py-2 text-white bg-black my-3' onClick={handleAddToCart}>Add to cart</button>
           </div>
-      </div>
-      </div>
-  )
-}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
